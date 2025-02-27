11194f88796c934ad250fb400ec11fe89b147e6d
RidDevs <127176942+RidDevs@users.noreply.github.com>
2025-02-27 21:18:13 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/chat"; // Make sure the endpoint is correct
+const API_URL = "https://skin-app-v5c1.onrender.com"; // Make sure the endpoint is correct
 
 document.addEventListener("DOMContentLoaded", function () {
   const inputField = document.getElementById("userInput");
