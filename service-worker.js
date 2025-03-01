6e47dfe7891631915c5501785167bfd0e0d68028
RidDevs <127176942+RidDevs@users.noreply.github.com>
2025-03-01 22:18:25 +0530

    yeah

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "skinwise-cache-v1";
+const urlsToCache = [
+  "/about.html",
+  "/index.html",
+  "/styles.css",
+  "/1000233440.png",
+  "/people.jpg"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached files
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
